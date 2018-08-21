69180f10bbf0309d1a9fa5fe186f0d71e519bb25
Mike Stucka <stucka@whitedoggies.com>
2018-08-21 10:29:23 -0400

    Initial commit

---
+++ resultsdownloader.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+import datetime
+import os
+
+
+# In[2]:
+
+
+subdir = "snapshots/"
+elexcode = "20180828"
+
+baseurl = "http://fldoselectionfiles.elections.myflorida.com/enightfilespublic/"
+
+filetypes = [
+    ('results', '_ElecResultsFL.txt'),
+    ('info', '_ElecResultsFL_PipeDlm_Info.txt'),
+    ('votes', '_ElecResultsFL_PipeDlm_Votes.txt')
+]
+
+# https://fl1dos.blob.core.windows.net/enightfilespublicdev/20180828_ElecResultsFL.txt
+# http://fldoselectionfiles.elections.myflorida.com/enightfilespublic/20180828_ElecResultsFL.txt
+# http://fldoselectionfiles.elections.myflorida.com/enightfilespublic/20180828_ElecResultsFL_PipeDlm_Info.txt
+# http://fldoselectionfiles.elections.myflorida.com/enightfilespublic/20180828_ElecResultsFL_PipeDlm_Votes.txt
+
+
+# In[3]:
+
+
+timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d-%H%M%S")
+
+
+# In[4]:
+
+
+path = subdir + timestamp + "/"
+print ("Saving to " + path)
+os.makedirs(path, exist_ok=True)
+for filetype in filetypes:
+    local, remote = filetype
+    with open(path + local + ".txt", "wb") as f:
+        f.write(requests.get(baseurl + elexcode + remote).content)
+with open(path + "done", "w", newline='') as f:
+    f.write("Start: " + timestamp + "\r\n")
+    f.write("End:   " + datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d-%H%M%S")  + "\r\n")
+print("Done")
+
