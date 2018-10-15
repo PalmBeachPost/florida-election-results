698d2c650499432e2757ca4daf92575c188b5b31
unknown <stucka@whitedoggies.com>
2018-10-15 15:10:04 -0400

    Parallelize download routines

---
+++ fetchallresults.py
@@ -0,0 +1,12 @@
+import os
+from multiprocessing import Pool
+
+processes = ("Florida.py", "PalmBeach.py")
+
+def run_process(process):
+    os.system('python {}'.format(process))
+    
+
+if __name__ ==  '__main__':
+    pool = Pool(processes=8)
+    pool.map(run_process, processes)
