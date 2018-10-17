1cf9309dd1fb94f3082b428af2dba80c91b39d05
unknown <stucka@whitedoggies.com>
2018-10-17 14:59:35 -0400

    Temporary move config to primary, not general, election:

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-electiondate = "11/6/2018"    # In m/d/yyyy format
+electiondate = "08/28/2018"    # In m/d/yyyy format
 snapshotsdir = "./snapshots/"       # path to save timestamped versions of downloaded data. Sample: "./snapshots/"
 targetdir = "../election-results-parser/rawscrapings/"
 # targetdir is the path to save parsed Elex-formatted results, such as "../election-results-parser/rawscrapings/"
