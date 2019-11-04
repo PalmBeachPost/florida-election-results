3e2cf6c809871e5b773c5d1072b6432c36e12117
Mike Stucka <stucka@whitedoggies.com>
2019-11-04 13:40:03 -0500

    Stage for debugging

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-electiondate = "04/09/2019"    # In m/d/yyyy format
+electiondate = "11/05/2019"    # In mm/dd/yyyy format
 snapshotsdir = "./snapshots/"       # path to save timestamped versions of downloaded data. Sample: "./snapshots/"
 targetdir = "../election-results-parser/rawscrapings/"
 # targetdir is the path to save parsed Elex-formatted results, such as "../election-results-parser/rawscrapings/"
