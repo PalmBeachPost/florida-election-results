0ad2c2fc565fe17a0d3ed29f6e834b149f626af0
Mike Stucka <stucka@whitedoggies.com>
2020-03-17 16:01:32 -0400

    Begin moving Palm Beach to new vendor style

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-electiondate = "11/05/2019"    # In mm/dd/yyyy format
+electiondate = "03/17/2020"    # In mm/dd/yyyy format
 snapshotsdir = "./snapshots/"       # path to save timestamped versions of downloaded data. Sample: "./snapshots/"
 targetdir = "../election-results-parser/rawscrapings/"
 # targetdir is the path to save parsed Elex-formatted results, such as "../election-results-parser/rawscrapings/"
